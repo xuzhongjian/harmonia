7c8ca8f5150775916740f99b2cc2ba8504ff1d03
Ganten Xu <zjxu97@gmail.com>
2025-03-18 00:05:39 +0800

    add balance transfer

--- contracts/CallerAddress.sol
+++ contracts/CallerAddress.sol
@@ -1,19 +1,27 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract CallerAddress {
 
-    uint constant one_eth = 1000000000000000000;
-
-    constructor(){
-    }
+    // 通过给合约注入一些以太币来确保有余额
+    constructor() payable {}
 
     function getCallerAddress() public view returns (address) {
         return msg.sender;
     }
 
     function transferToCaller() public {
-        address callerAddress = msg.sender;
-        callerAddress.transfer(one_eth * 100);
+        address payable callerAddress = payable(msg.sender);
+        uint amount = 100 ether;
+
+        // 确保合约中有足够余额
+        require(address(this).balance >= amount, "Insufficient balance in the contract");
+
+        callerAddress.transfer(amount);
+    }
+
+    // 获取合约余额
+    function getContractBalance() public view returns (uint) {
+        return address(this).balance;
     }
 }
--- test/CallerAddress.js
+++ test/CallerAddress.js
@@ -3,20 +3,26 @@ const { expect } = require("chai");
 
 let gantenAddress;
 let owner, user;  // 这里先声明变量
+const INIT_BALANCE = ethers.parseEther("1000");
 
 describe("CallerAddress", function () {
     before(async function () {
-        [owner, user] = await ethers.getSigners(); // 获取两个账户
+        // 获取两个账户
+        [owner, user] = await ethers.getSigners();
+
+        // 使用 owner 账户部署合约，并且给定一定的余额
         const CallerAddress = await ethers.getContractFactory("CallerAddress");
-        callerAddress = await CallerAddress.connect(owner).deploy();  // 用 owner 部署
+        callerAddress = await CallerAddress.connect(owner).deploy({ value: INIT_BALANCE });
         await callerAddress.waitForDeployment();
         console.log("gantenAddress deployed at:", callerAddress.target);
-        // 获取并打印余额
-        const ownerBalance = await owner.provider.getBalance(owner.address);
-        console.log("owner balance:", ownerBalance);
 
+        // 获取并打印两个账户的余额
+        const ownerBalance = await owner.provider.getBalance(owner.address);
+        console.log("start: owner balance:", ownerBalance);
         const userBalance = await user.provider.getBalance(user.address);
-        console.log("user balance:", userBalance);
+        console.log("start: user balance:", userBalance);
+        const contractBalance = await callerAddress.getContractBalance();
+        console.log("start: contract balance:", contractBalance);
     });
 
     it("step0: should return the owner address", async function () {
@@ -29,12 +35,17 @@ describe("CallerAddress", function () {
         expect(userAddress).to.equal(user.address);
     });
 
-    it("step2: should return user address", async function () {
-        const userAddress = await callerAddress.connect(user).transferToCaller();
-        const ownerBalance = await owner.provider.getBalance(owner.address);
-        console.log("owner balance:", ownerBalance);
+    it("step2: should transfer to the caller", async function () {
+        // 使用 user 去调用合约，合约会给 user 转账
+        const tx = await callerAddress.connect(user).transferToCaller();
+        await tx.wait();
 
+        // 打印最后的两个账户的余额
+        const ownerBalance = await owner.provider.getBalance(owner.address);
+        console.log("end: owner balance:", ownerBalance);
         const userBalance = await user.provider.getBalance(user.address);
-        console.log("user balance:", userBalance);
+        console.log("end: user balance:", userBalance);
+        const contractBalance = await callerAddress.getContractBalance();
+        console.log("end: contract balance:", contractBalance);
     });
 });
